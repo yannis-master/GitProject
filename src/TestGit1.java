3616f01a4ad14d053e09254aa36e33800ea2fc98
yannis <820495854@qq.com>
2018-09-06 08:56:36 +0800

    李四修改了内容

--- src/TestGit1.java
+++ src/TestGit1.java
@@ -2,6 +2,7 @@ public class TestGit1 {
     public static void main(String[] args){
         System.out.println("我爱java");
         System.out.println("我爱j");
+         System.out.println("李四修改了内容");
     }
 
 }
